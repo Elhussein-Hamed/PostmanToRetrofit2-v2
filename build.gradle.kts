8b702827d237f8168de7097dfe03ddd960887eec
Elhussein Hamed <36441143+Elhussein-Hamed@users.noreply.github.com>
2023-01-06 22:15:43 +0000

    Revert unnecessary changes to gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ version = "1.5.3-beta"
 
 repositories {
     mavenCentral()
-    maven { url "https://jitpack.io" }
 }
 
 // Configure Gradle IntelliJ Plugin
@@ -25,7 +24,6 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.fifesoft:rsyntaxtextarea:3.3.0")
-    implementation("com.github.Elhussein-Hamed:PostmanToRetrofit2-v2:1.5.2")
 }
 
 tasks {
